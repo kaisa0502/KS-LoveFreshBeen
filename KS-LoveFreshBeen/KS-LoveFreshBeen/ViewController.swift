8fdf55883989d3c6d0e2b643e80777974a222a5e
kaisa.huang <kaisa0502@163.com>
2016-06-01 23:30:57 +0800

    kaisa init

---
+++ KS-LoveFreshBeen/KS-LoveFreshBeen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KS-LoveFreshBeen
+//
+//  Created by kaisa.huang on 16/6/1.
+//  Copyright © 2016年 kaisa.huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ KS-LoveFreshBeen/Pods/Target Support Files/Pods-KS-LoveFreshBeen/Pods-KS-LoveFreshBeen-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KS_LoveFreshBeen : NSObject
+@end
+@implementation PodsDummy_Pods_KS_LoveFreshBeen
+@end
